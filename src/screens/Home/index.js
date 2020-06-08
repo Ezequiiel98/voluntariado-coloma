0b702439e25a5f970e4992b71a9916c363a3f3ea
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-06-08 01:23:25 -0300

    agrego styles a los buttons

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -12,10 +12,10 @@ export default function Home() {
         <div className={styles.container}>
           <p className={styles.description}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem laborum blanditiis vel placeat cum facere voluptas ea, nemo enim harum, aut quis. Alias molestiae culpa neque officiis iusto veniam dolore est consectetur vitae. Rerum fuga assumenda ad magnam in impedit laudantium reiciendis possimus, voluptatibus nam cumque aliquid sapiente repellendus aspernatur veritatis quaerat sunt, libero voluptas, ullam unde distinctio repellat doloremque. Nulla exercitationem quas laboriosam quos, tempora vitae praesentium molestias esse nisi explicabo voluptatum et. Quisquam enim perspiciatis unde dolores voluptatum harum earum qui quos similique rem dicta quo odit deserunt, dolorum provident aut aliquid in atque cum. Ducimus perferendis quidem vero blanditiis sunt eos quasi magnam aspernatur, eligendi illo harum adipisci provident voluptas voluptates asperiores error quod natus itaque consectetur! Nostrum delectus, sequi similique harum repudiandae laboriosam hic nam. Necessitatibus accusamus sapiente laboriosam nulla quisquam id. Quia, ad! Accusantium officiis at possimus tempora nam maiores architecto excepturi nostrum commodi eius. </p>
           <button className={styles.button} type="button">
-            Cómo separar
+            <span> Cómo separar  </span>
           </button>
           <button className={styles.button} type="button">
-            Dónde llevar los residuos
+            <span> Dónde llevar los residuos</span>
           </button>
         </div>
       </main>
