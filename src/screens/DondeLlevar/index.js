3ab1d5b283b2ce0431f3596cac6addd29e5803be
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-06-09 19:45:39 -0300

    agrego el mapa

---
+++ src/screens/DondeLlevar/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Map as MapContainer, Marker, TileLayer } from 'react-leaflet';
+import { markerIcon } from './components/markerIcon';
+import Title from '../../components/Title';
+
+import styles from './index.module.scss';
+
+
+export default function DondeLlevar() {
+ const position = [-34.6256097, -58.3808893, 21];
+ const position2 = [-34, -58.3808893, 21];
+
+return (
+  <section className={styles.dondeLlevar} > 
+    <Title Tag="h1" center text="¿DÓNDE LLEVO  MIS RESIDUOS?" />
+    <div className={styles.map}>
+      <MapContainer className={styles.map} center={position} zoom={18} scrollWheelZoom={false} >
+        <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={position2} icon={markerIcon('green')} />
+        <Marker position={position} icon={markerIcon('lightblue')} />
+
+      </MapContainer>
+    </div>
+  </section>
+ );
+}
+
