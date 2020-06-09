4259580f569bb1361a8ad27f1f31e0754079891b
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-06-09 15:38:25 -0300

    termino styles de sponsors

--- src/screens/Sponsors/index.js
+++ src/screens/Sponsors/index.js
@@ -7,12 +7,12 @@ import Sponsor from './components/Sponsor';
 import styles from './index.module.scss';
 
 export default function Sponsors() {
-      return (
-        <section>
-          <Title Tag="h1" text="¡CONOCE A QUIENES NOS APOYAN!" center className={styles.title} />
-          <div className={styles.sponsors}>
-            {SPONSORS.map(sponsor => <Sponsor key={sponsor.id} {...sponsor} />)}
-          </div>
-        </section>
-      );
+  return (
+    <section>
+      <Title Tag="h1" text="¡CONOCE A QUIENES NOS APOYAN!" center />
+      <div className={styles.sponsors}>
+        {SPONSORS.map(sponsor => <Sponsor key={sponsor.id} {...sponsor} />)}
+      </div>
+    </section>
+  );
 }
