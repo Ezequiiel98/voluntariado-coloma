04dc815dd9ca943ff88247860ac24c0bf0a0f977
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-06-10 02:45:10 -0300

    agrego el buscador de la localizacion

--- src/screens/DondeLlevar/index.js
+++ src/screens/DondeLlevar/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Map as MapContainer, Marker, TileLayer } from 'react-leaflet';
-import { markerIcon } from './components/markerIcon';
+import L from 'leaflet';
+
 import Title from '../../components/Title';
 
+import { markerIcon } from './components/markerIcon';
 import styles from './index.module.scss';
+import Geosearch from './search.js';
 
 
 export default function DondeLlevar() {
@@ -11,17 +14,18 @@ export default function DondeLlevar() {
  const position2 = [-34, -58.3808893, 21];
 
 return (
-  <section className={styles.dondeLlevar} > 
+  <section className={styles.dondeLlevar}>
     <Title Tag="h1" center text="¿DÓNDE LLEVO  MIS RESIDUOS?" />
     <div className={styles.map}>
-      <MapContainer className={styles.map} center={position} zoom={18} scrollWheelZoom={false} >
+      <MapContainer className={styles.map} center={position}    zoom={18} scrollWheelZoom={false}>
         <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
+	<Geosearch /> 
+
         <Marker position={position2} icon={markerIcon('green')} />
         <Marker position={position} icon={markerIcon('lightblue')} />
-
       </MapContainer>
     </div>
   </section>
