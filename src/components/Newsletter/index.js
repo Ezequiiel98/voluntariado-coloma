c527a60df7fe3947218fa0ea24409715ef088432
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-06-12 00:45:15 -0300

    corrijo botones y agrego estilos

--- src/components/Newsletter/index.js
+++ src/components/Newsletter/index.js
@@ -5,14 +5,14 @@ import Title from '../Title';
 import styles from './index.module.scss';
 
 export default function Newsletter() {
-      return (
-        <div className={styles.newsletter}>
-          <Title Tag="h2" text="¡NO TE PIERDAS TIPS E IDEAS PARA SER MAS ECONSIENTE!" />
-          <form className={styles.containerForm}>
-            <input type="email" required placeholder="dejá@tu.mail" className={styles.error} />
-            <button type="submit" className={styles.button}> + ME SUMO</button>
-          </form>
-          <p className={styles.place}>Miramar, Córdoba, Argentina.</p>
-        </div>
-      );
+  return (
+    <div className={styles.newsletter}>
+      <Title Tag="h2" text="¡NO TE PIERDAS TIPS E IDEAS PARA SER MAS ECONSIENTE!" />
+      <form className={styles.containerForm}>
+        <input type="email" required placeholder="dejá@tu.mail" className={styles.input} />
+        <button type="submit" className={styles.button}> + ME SUMO</button>
+      </form>
+      <p className={styles.place}>Miramar, Córdoba, Argentina.</p>
+    </div>
+  );
 }
--- src/screens/Home/components/Button/index.js
+++ src/screens/Home/components/Button/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import styles from './index.module.scss';
 
-export default function Button({ onClick, text }) {
+export default function Button({ text, path }) {
   return (
     <button className={styles.button} type="button">
-      {text}
+      <Link to={path} className={styles.link}>
+        {text}
+      </Link>
     </button>
   );
 }
