000e10e99c029c50f02f118dc5642661369adbcc
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-06-12 02:14:01 -0300

    agrego react lazy

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Route, Switch, Redirect } from 'react-router';
 
-import ComoSeparar from '../../screens/ComoSeparar';
-import DondeLlevar from '../../screens/DondeLlevar';
-import Sponsors from '../../screens/Sponsors';
-import Home from '../../screens/Home';
-
+import ComoSeparar from '../../screens/ComoSeparar'; 
+   import DondeLlevar from '../../screens/DondeLlevar';
+ import Home from '../../screens/Home';
+  import Sponsors from '../../screens/Sponsors';
+  import Contacto from '../../screens/Contacto'; 
+/*
+const Home = lazy(() => import('../../screens/Home'));
+const ComoSeparar = lazy(() => import('../../screens/ComoSeparar'));
+const DondeLlevar = lazy(() => import('../../screens/DondeLlevar'));
+const Sponsors = lazy(() => import('../../screens/Sponsors'));
+const Contacto = lazy(() => import(/*webpackChunckName: "contacto"'../../screens/Contacto'));
+*/
 export default function Main() {
   return (
     <main>
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/como-separar" component={ComoSeparar} />
-        <Route exact path="/donde-llevar" component={DondeLlevar} />
-        <Route exact path="/sponsors" component={Sponsors} />
-        <Route path="/" render={()=><Redirect to="/" />} />
+        <Suspense fallback={<div>Cargando...</div>}>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/como-separar" getComponent={(location, cb) => { cb(null, ComoSeparar) } } />
+	 
+          <Route exact path="/donde-llevar" component={DondeLlevar} />
+          <Route exact path="/sponsors" component={Sponsors} />
+          <Route exact path="/contacto" component={Contacto} />
+
+          <Route path="/" render={()=><Redirect to="/" />} />
+        </Suspense>
       </Switch>
     </main>
   );
