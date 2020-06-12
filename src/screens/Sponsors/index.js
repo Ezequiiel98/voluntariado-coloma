f90bb8dca8f8f7a4b02c8b19e1248e9f1d2339f2
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-06-12 02:42:05 -0300

    agrego jsconfig

--- src/screens/Sponsors/index.js
+++ src/screens/Sponsors/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Title from '../../components/Title';
+import Title from 'components/Title';
 
 import { SPONSORS } from './constants/sponsors';
 import Sponsor from './components/Sponsor';
