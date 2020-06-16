b8c5d9b3fa6db2b50bf0fa360a820920e82d7285
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-06-16 03:09:22 -0300

    borro archivos innecesarios

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,25 +1,24 @@
 import React, { Suspense, lazy } from 'react';
 import { Route, Switch, Redirect } from 'react-router';
-
+/*
 import ComoSeparar from '../../screens/ComoSeparar'; 
    import DondeLlevar from '../../screens/DondeLlevar';
  import Home from '../../screens/Home';
   import Sponsors from '../../screens/Sponsors';
   import Contacto from '../../screens/Contacto'; 
-/*
+*/
 const Home = lazy(() => import('../../screens/Home'));
 const ComoSeparar = lazy(() => import('../../screens/ComoSeparar'));
 const DondeLlevar = lazy(() => import('../../screens/DondeLlevar'));
 const Sponsors = lazy(() => import('../../screens/Sponsors'));
-const Contacto = lazy(() => import(/*webpackChunckName: "contacto"'../../screens/Contacto'));
-*/
+const Contacto = lazy(() => import('../../screens/Contacto')); 
 export default function Main() {
   return (
     <main>
       <Switch>
         <Suspense fallback={<div>Cargando...</div>}>
           <Route exact path="/" component={Home} />
-          <Route exact path="/como-separar" getComponent={(location, cb) => { cb(null, ComoSeparar) } } />
+          <Route exact path="/como-separar" component={ComoSeparar} />
 	 
           <Route exact path="/donde-llevar" component={DondeLlevar} />
           <Route exact path="/sponsors" component={Sponsors} />
