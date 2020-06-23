db5528fa4c4afe65e4fb63dead1243b6c4b920ca
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-06-23 05:35:22 -0300

    agregue las redes, falta dividir componentes poner iconos y links

--- src/screens/Contacto/index.js
+++ src/screens/Contacto/index.js
@@ -7,25 +7,40 @@ import styles from './index.module.scss';
 export default function Contacto() {
   return (<>
     <Title Tag="h1" text="¡CONTACTANOS!" center />
-    <Title Tag="h3" text="Hola! Si deseas contactarte déjanos un mensaje aquí o en nuestras redes sociales" className={styles.subtitle} />
+    <Title Tag="h3" text="¡Hola! Si deseas contactarte déjanos un mensaje aquí o en nuestras redes sociales." className={styles.subtitle} />
     <div className={styles.container}>
       <form className={styles.form}>
         <div className={styles.containerInput}>
           <label className={styles.label} htmlFor="name"> Nombre: </label>
-          <input className={styles.input} type="text" id="name" placeholder="Nombre" />
+          <input className={styles.input} type="text" id="name" placeholder="Nombre" required />
         </div>
         <div className={styles.containerInput}>
           <label className={styles.label} htmlFor="email"> Correo electrónico: </label>
-          <input className={styles.input} type="email" id="email" placeholder="Correo electrónico" />
+          <input className={styles.input} type="email" id="email" placeholder="Correo electrónico" required />
         </div>
         <div className={styles.containerTextarea}>
           <label className={styles.label} htmlFor="message"> Mensaje:  </label>
-          <textarea className={styles.textarea} type="text" id="message" placeholder="Escribe aquí tu mensaje" />
+          <textarea className={styles.textarea} type="text" id="message" placeholder="Escribe aquí tu mensaje" required />
         </div>
-        <button className={styles.button} type="submit">Enviar</button>
+        <button disabled className={styles.button} type="submit">Enviar</button>
       </form>
-      <div>
-        <p> Weno aca van las redes sociales </p>
+      <div className={styles.containerSocialNet}>
+        <div className={styles.socialNet}>
+          <div className={styles.iconSocialNet} />
+          <p className={styles.userSocialNet}>@proyectoeconsientes</p>
+        </div>
+        <div className={styles.socialNet}>
+          <div className={styles.iconSocialNet} />
+          <p className={styles.userSocialNet}>@proyectoeconsientes</p>
+        </div>
+        <div className={styles.socialNet}>
+          <div className={styles.iconSocialNet} />
+          <p className={styles.userSocialNet}>@proyectoeconsientes</p>
+        </div>
+        <div className={styles.socialNet}>
+          <div className={styles.iconSocialNet} />
+          <p className={styles.userSocialNet}>@proyectoeconsientes</p>
+        </div>
       </div>
 
     </div>
