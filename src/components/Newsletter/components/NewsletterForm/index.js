fe692b58628fa281a0bebf2a7a30c84e73cf8982
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-06-27 01:54:33 -0300

    agrego formik al form del newsletter0

---
+++ src/components/Newsletter/components/NewsletterForm/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {Formik, Form, Field, ErrorMessage} from 'formik';
+import styles from './index.module.scss';
+
+const validate = ({ email }) => {
+  const errors = {};
+  const expRegEmail = /(^[0-9a-zA-Z]+[-._+&])*[0-9a-zA-Z]+@([-0-9a-zA-Z]+[.])+[a-zA-Z]{2,6}$/;
+
+  if(!email){
+    errors.email = "Debe ingresar el email";
+  }else if (!expRegEmail.test(email)){
+    errors.email = 'El correo electrónico es invalido';
+  }
+  
+  return errors;
+}
+
+const onSubmit = (values, onSubmitProps) => {
+  console.log(values);
+  onSubmitProps.setSubmitting(false);
+}
+
+export default function NessletterForm() {
+  return(
+    <Formik
+      initialValues={ {email: ''} }
+      validate={validate}
+      onSubmit={onSubmit}
+      validateOnMount
+    >
+      {formik => (
+	<Form className={styles.containerForm}>
+	  <Field name="email">
+	    { ({ field, meta }) => {
+	      const className = meta.touched && meta.error ? styles.inputError : styles.input;
+	      return <input {...field} className={className} type="email" placeholder="dejá@tu.mail" />;
+	      }
+	      }
+	    </Field>
+	    <button type="submit" className={styles.button} disabled={!formik.isValid || formik.isSubmitting}> + ME SUMO</button>
+	 </Form>
+      )} 
+    </Formik>  
+  )}
--- src/components/Newsletter/index.js
+++ src/components/Newsletter/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
 
 import Title from '../Title';
-
+import NewsletterForm from './components/NewsletterForm';
+ 
 import styles from './index.module.scss';
 
 export default function Newsletter() {
   return (
     <div className={styles.newsletter}>
       <Title Tag="h2" text="¡NO TE PIERDAS TIPS E IDEAS PARA SER MAS ECONSIENTE!" />
-      <form className={styles.containerForm}>
-        <input type="email" required placeholder="dejá@tu.mail" className={styles.input} />
-        <button type="submit" className={styles.button}> + ME SUMO</button>
-      </form>
+      <NewsletterForm />
       <p className={styles.place}>Miramar, Córdoba, Argentina.</p>
-    </div>
+   </div>
   );
 }
