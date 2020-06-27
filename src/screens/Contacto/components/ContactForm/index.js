711a846840103fde4ffcb97c03614b4ad500c38b
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-06-27 01:12:39 -0300

    mejoro la validacion del form y edito estilos dalnetworks

--- src/screens/Contacto/components/ContactForm/index.js
+++ src/screens/Contacto/components/ContactForm/index.js
@@ -8,24 +8,33 @@ const initialValues = { name: '', email: '', message: '' };
 
 const validate = ({ name, email, message }) => {
   const errors = {};
+  const expRegEmail = /(^[0-9a-zA-Z]+[-._+&])*[0-9a-zA-Z]+@([-0-9a-zA-Z]+[.])+[a-zA-Z]{2,6}$/;
 
   if (!name) {
-    errors.name = 'El campo nombre es obligatorio';
+    errors.name = 'Este campo es obligatorio';
   }
 
   if (!email) {
-    errors.email = 'El campo nombre es obligatorio';
+    errors.email = 'Este campo es obligatorio';
+  }else if (!expRegEmail.test(email)){
+    errors.email = 'El correo electrónico es invalido';
   }
 
   if (!message) {
-    errors.message = 'El campo nombre es obligatorio';
+    errors.message = 'Este campo obligatorio';
   }
 
   return errors;
 };
 
-const onSubmit = values => console.log(values);
-
+const onSubmit = (values, onSubmitProps) => {
+  console.log('pedrito');
+  setTimeout( ()=>{
+    console.log('juanchilopeisn')
+  onSubmitProps.setSubmitting(false);
+  }, 2000);
+  
+}
 
 export default function ContactForm() {
   return (
@@ -33,45 +42,50 @@ export default function ContactForm() {
       initialValues={initialValues}
       validate={validate}
       onSubmit={onSubmit}
+      validateOnMount
     >
-      <Form className={styles.form}>
-	<div className={styles.containerInput}>
-	  <label className={styles.label} htmlFor="name"> Nombre: </label>
-	  <Field name="name">
-	    { ({ field, meta }) => {
-	      const className = meta.touched && meta.error ? styles.inputError : styles.input;
-	      return <input {...field} className={className} type="text" id="name" placeholder="Nombre..." />;
-	      }
-	      }
-	    </Field>
-	    <ErrorMessage name="name" component={Error} />
-	  </div>
-	  <div className={styles.containerInput}> 
-	    <label className={styles.label} htmlFor="email"> Correo electrónico: </label>
-	    <Field name="email">
-	      { ({ field, meta }) => {
-		const className = meta.touched && meta.error ? styles.inputError : styles.input;
-		return <input {...field} className={className} type="email" id="email" placeholder="Correo electrónico..." />;
-		}
-		}
-	      </Field>
-
-	      <ErrorMessage name="email" component={Error} />
-	    </div>
-	    <div className={styles.containerTextarea}>
-	      <label className={styles.label} htmlFor="message"> Mensaje:  </label>
-	      <Field name="message">
+      {formik => {
+	return(
+	  <Form className={styles.form}>
+	    <div className={styles.containerInput}>
+	      <label className={styles.label} htmlFor="name"> Nombre: </label>
+	      <Field name="name">
 		{ ({ field, meta }) => {
-		  const className = meta.touched && meta.error ? styles.textareaError : styles.textarea;
-		  return <textarea {...field} className={className} id="message" placeholder="Mensaje..." />;
+		  const className = meta.touched && meta.error ? styles.inputError : styles.input;
+		  return <input {...field} className={className} type="text" id="name" placeholder="Nombre..." />;
 		  }
 		  }
 		</Field>
-
-		<ErrorMessage name="message" component={Error} />
+		<ErrorMessage name="name" component={Error} />
 	      </div>
-	      <button className={styles.button} type="submit">Enviar</button>
-	    </Form>
-	  </Formik>
+	      <div className={styles.containerInput}> 
+		<label className={styles.label} htmlFor="email"> Correo electrónico: </label>
+		<Field name="email">
+		  { ({ field, meta }) => {
+		    const className = meta.touched && meta.error ? styles.inputError : styles.input;
+		    return <input {...field} className={className} type="email" id="email" placeholder="Correo electrónico..." />;
+		    }
+		    }
+		  </Field>
+
+		  <ErrorMessage name="email" component={Error} />
+		</div>
+		<div className={styles.containerTextarea}>
+		  <label className={styles.label} htmlFor="message"> Mensaje:  </label>
+		  <Field name="message">
+		    { ({ field, meta }) => {
+		      const className = meta.touched && meta.error ? styles.textareaError : styles.textarea;
+		      return <textarea {...field} className={className} id="message" placeholder="Mensaje..." />;
+		      }
+		      }
+		    </Field>
+
+		    <ErrorMessage name="message" component={Error} />
+		  </div>
+		  <button className={styles.button} type="submit" disabled={!formik.isValid || formik.isSubmitting}>Enviar</button>
+		</Form>
+	  )
+      }}
+    </Formik>
   );
 }
